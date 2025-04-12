7219eb7a556f76379f59fc425202e5f06ca407f0
Anshul <avenger720720@gmail.com>
2025-04-12 13:22:24 +0530

    working on functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+const time = document.getElementById('time');
+const timeformat = document.getElementById('timeformat')
+
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(showTime ,1000);
+});
+
+const showTime = () => {
+    let date = new Date();
+
+    let hr = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds()
+
+}
