6d44bdaa467e922282b9b94a12a03b6082d63391
Anshul <avenger720720@gmail.com>
2025-04-14 16:48:42 +0530

    completed

--- script.js
+++ script.js
@@ -12,4 +12,10 @@ const showTime = () => {
     let mins = date.getMinutes();
     let secs = date.getSeconds()
 
+    hr = hr<10 ? `0${hr}` : hr;
+    mins = mins<10 ? `0${mins}` : mins;
+    secs = secs<10 ? `0${secs}` : secs;
+    time.innerHTML = `${hr} : ${mins} : ${secs}`
+
+    timeformat.innerHTML = hr>12 ? "PM" : "AM";
 }
